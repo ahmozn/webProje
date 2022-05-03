42ae8b45ccfc56db2c9b46e75ed392f0838fc648
Ozhan <79666439+ahmozn@users.noreply.github.com>
2022-05-03 21:23:29 +0300

    Add files via upload

---
+++ iletisim.php
@@ -0,0 +1,24 @@
+<?php
+    if (isset($_POST['onay']))
+    {
+        if (isset($_POST['ad'], $_POST['soyad'] )&& ($_POST['ad']!='')&& ($_POST['soyad']!=''))
+        {
+            echo "Ad: ".''.$_POST['ad'];
+            echo "</br>";
+            echo "Soyad: ".''.$_POST['soyad'];
+            echo "</br>";
+            echo "Cinsiyet: ".''.$_POST['cinsiyet'];
+            echo "</br>";
+            echo "E-mail: ".''.$_POST['email'];
+            echo "</br>";
+            echo "Şifre: ".''.$_POST['sifre'];
+            echo "</br>";
+            echo "Okuduğunuz Bölüm: ".''.$_POST['bolumler'];
+            echo "</br>";
+            echo "Adres: ".''.$_POST['textarea'];
+
+        }
+        
+    }
+    echo "<p> <a href='iletisim.html'>&lt;GERİ DÖN&gt;</a></p> ";
+?>
